71de6ed5d8afab973b89dfc22af003d98bf0e2bc
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-07-16 13:42:07 +0000

    Add #new, #create and controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to edit_user_path
+    else
+      flash.now[:error] = "Rats! Fix your mistakes, please."
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
