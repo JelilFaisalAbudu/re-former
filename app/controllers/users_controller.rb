f4b629f1eae09c647af6e43ea7f21dca0dc021d5
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-07-17 06:12:39 +0000

    Add #edit and #update action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def index
-    @user = User.all
-  end
-
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
     if @user.save
-      redirect_to edit_user_path
+      redirect_to new_user_path
     else
-      flash.now[:error] = "Rats! Fix your mistakes, please."
+      flash.now[:error] = 'Rats! Fix your mistakes, please.'
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
     end
   end
 
